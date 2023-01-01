054a7f067923a434691f045485af8a1639c0a0d6
ZeCompadre <zecompadre@users.noreply.github.com>
2023-01-01 17:22:54 +0000

    Update cookidoo_collection.js

--- cookidoo_collection.js
+++ cookidoo_collection.js
@@ -2,69 +2,49 @@
 (function() {
     'use strict';
 
-    var copyToCipboard = function(text)
+    var holder = document.querySelector(".core-nav__container .core-nav__main-links.authenticated-only");
+    if(holder)
     {
-        var textarea = document.createElement("textarea");
-        textarea.textContent = text;
-        textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in MS Edge.
-        document.body.appendChild(textarea);
-        textarea.select();
-        document.execCommand("copy");
-        document.body.removeChild(textarea);
+        var li = document.createElement("li");
+        li.classList.add('core-nav__item');
+        li.appendBefore(holder);
+        holder.insertBefore(li, holder.firstChild);
+
+        var a = document.createElement("a");
+
+        a.innerText = 'Copiar';
+        a.classList.add('core-nav__link');
+        a.setAttribute("href", "#");
+        li.appendChild(a);
+
+        a.onclick = function() {
+
+            var allText = [];
+
+            var title = document.querySelector('.collection-header__title');
+            allText.push("Cookidoo®: Colecção \"" + title.innerText + "\"");
+            allText.push('');
+            var number = document.querySelector('.collection-header__subtitle');
+            allText.push(number.innerText);
+            allText.push('');
+            var description = number.nextElementSibling;
+            allText.push(description.innerText);
+            allText.push('');
+            allText.push('Lista');
+            allText.push('');
+            var recipies = document.querySelectorAll('core-tiles-list p.core-tile__description-text');
+            recipies.forEach(li => {
+                allText.push(li.innerText);
+            });
+            allText.push('');
+            allText.push("[cookidoo url='"+window.location.href+"']");
+
+            console.clear();
+
+            console.log(allText.join('\n'));
+
+            copyToCipboard(allText.join('\n'));
+            return false;
+        };
     }
-
-    Element.prototype.appendAfter = function (element) {
-        element.parentNode.insertBefore(this, element.nextSibling);
-    };
-
-    Element.prototype.appendBefore = function (element) {
-        element.parentNode.insertBefore(this, element);
-    };
-
-    var holder = document.querySelector(".l-content.l-header-offset.collection-header > .g-wrapper");
-
-    document.querySelector(".l-header-offset").style.marginTop = "5.5rem";
-
-    var button = document.createElement("span");
-    button.classList.add('button');
-    button.classList.add('button--primary');
-    button.innerText = 'Copiar';
-    button.style.textAlign = "center";
-    button.style.marginBottom = "10px";
-    //button.style.width = "100%";
-    button.style.position = "fixed";
-    button.style.zIndex  = "9999";
-
-    button.appendBefore(holder);
-
-    button.onclick = function() {
-
-        var allText = [];
-        
-        var title = document.querySelector('.collection-header__title');
-        allText.push("Cookidoo®: Colecção \"" + title.innerText + "\"");
-        allText.push('');
-        var number = document.querySelector('.collection-header__subtitle');
-        allText.push(number.innerText);
-        allText.push('');
-        var description = number.nextElementSibling;
-        allText.push(description.innerText);
-        allText.push('');
-        allText.push('Lista');
-        allText.push('');
-        var recipies = document.querySelectorAll('core-tiles-list p.core-tile__description-text');
-        recipies.forEach(li => {
-            allText.push(li.innerText);
-        });
-        allText.push('');
-        allText.push("[cookidoo url='"+window.location.href+"']");
-
-        console.clear();
-
-        console.log(allText.join('\n'));
-
-        copyToCipboard(allText.join('\n'));
-        return false;
-    };
-
 })();
