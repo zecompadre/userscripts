be2e850c464a18217b7676726d2d8dec5a296582
ZeCompadre <zecompadre@gmail.com>
2023-01-16 19:54:44 +0000

    ongoing

--- bimbyworld.js
+++ bimbyworld.js
@@ -1,12 +1,19 @@
-(function() {
-    'use strict';
+(function () {
+	'use strict';
 
-    var title = jQuery(this).text();
-    var socialtocopy = jQuery('#h_socialtocopy').val().replaceAll(/\[br\]/ig, '\n');
-    var link = jQuery('link[rel="canonical"').attr("href");
+	var holder = jQuery(".post-title.entry-title");
 
-    console.clear();
-    console.log("\n\nVeja aqui: " + link + "\n\n" + socialtocopy + "\n\nVeja aqui: " + link);
+	var button = document.createElement("button");
+	button.innerText = 'Copiar';
+	button.classList.add('button--primary');
+	holder.appendChild(button);
 
-    copyToCipboard("\n\nVeja aqui: " + link + "\n\n" + socialtocopy + "\n\nVeja aqui: " + link);
+	var title = holder.text();
+	var socialtocopy = jQuery('#h_socialtocopy').val().replaceAll(/\[br\]/ig, '\n');
+	var link = jQuery('link[rel="canonical"').attr("href");
+
+	console.clear();
+	console.log("\n\nVeja aqui: " + link + "\n\n" + socialtocopy + "\n\nVeja aqui: " + link);
+
+	copyToCipboard("\n\nVeja aqui: " + link + "\n\n" + socialtocopy + "\n\nVeja aqui: " + link);
 })();
