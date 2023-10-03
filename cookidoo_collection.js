7292dd2539dad9b010ab6c5a3bf612626902439e
ZeCompadre <zecompadre@gmail.com>
2023-10-03 19:35:49 +0100

    ongoing

--- cookidoo_collection.js
+++ cookidoo_collection.js
@@ -1,45 +1,42 @@
 (function () {
-    'use strict';
-    var holder = document.querySelector(".core-nav__container .core-nav__main-links.authenticated-only");
-    if (holder) {
-        var li = document.createElement("li");
-        li.classList.add('core-nav__item');
-        li.appendBefore(holder);
-        holder.insertBefore(li, holder.firstChild);
+	'use strict';
+	var holder = document.querySelector(".core-nav__container .core-nav__main-links.authenticated-only");
+	if (holder) {
+		var button = document.createElement("button");
+		button.classList.add('button-copy');
+		button.setAttribute('role', 'button');
+		button.innerText = 'Copiar';
 
-        var button = document.createElement("button");
-        button.innerText = 'Copiar';
-        button.classList.add('button--primary');
-        li.appendChild(button);
+		document.body.appendChild(button);
 
-        button.onclick = function () {
+		button.onclick = function () {
 
-            var allText = [];
+			var allText = [];
 
-            var title = document.querySelector('.collection-header__title');
-            allText.push("Cookidoo®: Colecção \"" + title.innerText + "\"");
-            allText.push('');
-            var number = document.querySelector('.collection-header__subtitle');
-            allText.push(number.innerText);
-            allText.push('');
-            var description = number.nextElementSibling;
-            allText.push(description.innerText);
-            allText.push('');
-            allText.push('Lista');
-            allText.push('');
-            var recipies = document.querySelectorAll('core-tiles-list p.core-tile__description-text');
-            recipies.forEach(li => {
-                allText.push(li.innerText);
-            });
-            allText.push('');
-            allText.push("[cookidoo url='" + window.location.href + "']");
+			var title = document.querySelector('.collection-header__title');
+			allText.push("Cookidoo®: Colecção \"" + title.innerText + "\"");
+			allText.push('');
+			var number = document.querySelector('.collection-header__subtitle');
+			allText.push(number.innerText);
+			allText.push('');
+			var description = number.nextElementSibling;
+			allText.push(description.innerText);
+			allText.push('');
+			allText.push('Lista');
+			allText.push('');
+			var recipies = document.querySelectorAll('core-tiles-list p.core-tile__description-text');
+			recipies.forEach(li => {
+				allText.push(li.innerText);
+			});
+			allText.push('');
+			allText.push("[cookidoo url='" + window.location.href + "']");
 
-            console.clear();
+			console.clear();
 
-            console.log(allText.join('\n'));
+			console.log(allText.join('\n'));
 
-            copyToCipboard(allText.join('\n'));
-            return false;
-        };
-    }
+			copyToCipboard(allText.join('\n'));
+			return false;
+		};
+	}
 })();
