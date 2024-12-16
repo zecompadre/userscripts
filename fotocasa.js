bb04c333c3022d522aebc995a58523598d586704
Luis Romão <luis.romao@janeladigital.com>
2024-12-16 15:00:02 +0000

    Refactor geoJsonToWKT function to enhance code readability and maintainability

--- fotocasa.js
+++ fotocasa.js
@@ -69,7 +69,7 @@ function geoJsonToWKT(geoJson) {
 
 		button.onclick = function (e) {
 
-			var location  =		JSON.parse(localStorage.getItem('LatestsSearches'))[0].combinedLocationIds.replace(/,/g, '_')
+			var location  =	JSON.parse(localStorage.getItem('LatestsSearches'))[0].combinedLocationIds.replace(/,/g, '_')
 
 		var url = "https://geom.fotocasa.es/v104/geom_" + location + ".js";
 		var xhr = new XMLHttpRequest();
@@ -79,72 +79,72 @@ function geoJsonToWKT(geoJson) {
 			if (xhr.readyState === XMLHttpRequest.DONE) {
 				if (xhr.status === 200) {
 
-			var jsonstr  = xhr.responseText.replace(/(var(:?.*)geom_(:?.*)(?:\s)\=(?:\s))+/gm, "");
+					var jsonstr  = xhr.responseText.replace(/(var(:?.*)geom_(:?.*)(?:\s)\=(?:\s))+/gm, "");
 
-			var feature = JSON.parse(jsonstr);
+					var feature = JSON.parse(jsonstr);
 
-			var wkt = geoJsonToWKT(feature);
-			console.log(wkt);
+					var wkt = geoJsonToWKT(feature);
+					console.log(wkt);
 
-			copyToCipboard(wkt);
+					copyToCipboard(wkt);
 
-			var bc = document.querySelectorAll(".breadcrumb-navigation > li > a");
-			var cur = document.querySelector(".breadcrumb-navigation-current-level");
+					var bc = document.querySelectorAll(".breadcrumb-navigation > li > a");
+					var cur = document.querySelector(".breadcrumb-navigation-current-level");
 
-			if (bc.length === 0) {
-				bc = document.querySelectorAll(".breadcrumb-geo > ul > li > a");
-				cur = document.querySelector(".breadcrumb-geo > ul > li:last-child");
-			}
+					if (bc.length === 0) {
+						bc = document.querySelectorAll(".breadcrumb-geo > ul > li > a");
+						cur = document.querySelector(".breadcrumb-geo > ul > li:last-child");
+					}
 
-			var text = "Zona visível";
-			if (bc) {
-				var bcText = [];
-				bc.forEach(a => {
-					bcText.push(a.innerText);
-				});
-				if (cur) {
-					bcText.push(cur.innerText);
-				}
-				if (bcText.length > 0) {
-					text = bcText.join(", ");
-				}
-			}
+					var text = "Zona visível";
+					if (bc) {
+						var bcText = [];
+						bc.forEach(a => {
+							bcText.push(a.innerText);
+						});
+						if (cur) {
+							bcText.push(cur.innerText);
+						}
+						if (bcText.length > 0) {
+							text = bcText.join(", ");
+						}
+					}
 
-			var msg = document.getElementById("map-undo-message");
-			if (msg) {
-				msg.remove();
-			}
+					var msg = document.getElementById("map-undo-message");
+					if (msg) {
+						msg.remove();
+					}
 
-			msg = document.createElement("div");
-			msg.id = "map-shape-message";
-			msg.classList.add("map-message");
-			msg.classList.add("feedback");
-			msg.classList.add("contextual");
-			msg.classList.add("warning");
-			msg.style.width = "100%";
-			msg.style.textAlign = "center";
-			msg.innerHTML = "SHAPE " + titleCase(text) + ", copiada para o clipboard<br />Verificar -> <a id='shapetest' href='" + validator + "' target='_shapetest'> Shape Test!</a>";
+					msg = document.createElement("div");
+					msg.id = "map-shape-message";
+					msg.classList.add("map-message");
+					msg.classList.add("feedback");
+					msg.classList.add("contextual");
+					msg.classList.add("warning");
+					msg.style.width = "100%";
+					msg.style.textAlign = "center";
+					msg.innerHTML = "SHAPE " + titleCase(text) + ", copiada para o clipboard<br />Verificar -> <a id='shapetest' href='" + validator + "' target='_shapetest'> Shape Test!</a>";
 
-			var close = document.createElement("a");
-			close.classList.add("icon-close");
-			close.classList.add("close-btn");
-			close.style.cursor = "pointer";
-			close.style.position = "absolute";
-			close.style.right = "5px";
-			close.style.top = "5px";
+					var close = document.createElement("a");
+					close.classList.add("icon-close");
+					close.classList.add("close-btn");
+					close.style.cursor = "pointer";
+					close.style.position = "absolute";
+					close.style.right = "5px";
+					close.style.top = "5px";
 
-			close.onclick = closeMsg;
+					close.onclick = closeMsg;
 
-			msg.appendChild(close);
+					msg.appendChild(close);
 
-			var map = document.getElementById("map-placeholder");
+					var map = document.getElementById("map-placeholder");
 
-			var shapeclose = document.querySelector(".delete-cross-button.icon-close");
-			if (shapeclose) {
-				document.querySelector(".delete-cross-button.icon-close").onclick = closeMsg;
-			}
+					var shapeclose = document.querySelector(".delete-cross-button.icon-close");
+					if (shapeclose) {
+						document.querySelector(".delete-cross-button.icon-close").onclick = closeMsg;
+					}
 
-			map.insertBefore(msg, map.firstChild);
+					map.insertBefore(msg, map.firstChild);
 
 
 				} else {
