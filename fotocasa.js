ef38fd7e63a187aab096543f04e5615038698bd8
Luis Romão <luis.romao@janeladigital.com>
2024-12-16 11:45:32 +0000

    Refactor request interception to use fetch API and log requests and responses

--- fotocasa.js
+++ fotocasa.js
@@ -5,25 +5,25 @@
 
 
 	(function() {
-		const originalOpen = XMLHttpRequest.prototype.open;
-		const originalSend = XMLHttpRequest.prototype.send;
+		const originalFetch = window.fetch;
 	
-		XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-			this._url = url; // Store the URL for later use
-			return originalOpen.apply(this, arguments);
+		window.fetch = async function(resource, init) {
+			console.log('Intercepted Request:', resource);
+			if (init) {
+				console.log('Request Options:', init);
+			}
+	
+			// Optionally modify the request or options
+			const response = await originalFetch(resource, init);
+	
+			// Intercept and optionally modify the response
+			const clonedResponse = response.clone();
+			clonedResponse.text().then((text) => {
+				console.log('Response:', text);
+			});
+	
+			return response;
 		};
-	
-		XMLHttpRequest.prototype.send = function(body) {
-			console.log(`Intercepted Request: ${this._url}`);
-			console.log('Request Body:', body);
-			
-			// Optionally modify the request body here
-			return originalSend.apply(this, arguments);
-		};
-	
-		this.addEventListener('load', function() {
-			console.log(`Response for ${this._url}:`, this.responseText);
-		});
 	})();
 
 	window.addEventListener('load', function () {
