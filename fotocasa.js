37c953c4d4780b837d1c889a3588ee8fc308e446
Luis Romão <luis.romao@janeladigital.com>
2024-12-16 15:30:42 +0000

    Refactor fetch URL handling and clean up commented code in fotocasa.js for improved readability

--- fotocasa.js
+++ fotocasa.js
@@ -2,52 +2,22 @@
 
 	//var validator = "https://wkt-playground.zecompadre.com";
 	var validator = "https://wkt-plotter.zecompadre.com";
+	var fetchurl = "https://geom.fotocasa.es/v104/geom_[location].js";
 
-/*
-	(function() {
-		const originalFetch = window.fetch;
-	
-		window.fetch = async function(resource, init) {
-			console.log('Intercepted Request:', resource);
-			console.log('Intercepted Request URL:', (resource.url || ''));
-			var isgeo =(resource.url || '').indexOf("/geo_") > -1;
-			if(isgeo)
-			{
 
-			if (init) {
-				console.log('Request Options:', init);
-			}
+	function geoJsonToWKT(geoJson) {
+		if (geoJson.geometry.type === "Polygon") {
+			// For Polygon, just join the coordinates in WKT format
+			return "POLYGON ((" + geoJson.geometry.coordinates[0].map(coord => coord.join(" ")).join(", ") + "))";
+		} else if (geoJson.geometry.type === "MultiPolygon") {
+			// For MultiPolygon, loop through each polygon and join coordinates in WKT format
+			return "MULTIPOLYGON ((" + geoJson.geometry.coordinates.map(polygon =>
+				"(" + polygon[0].map(coord => coord.join(" ")).join(", ") + ")"
+			).join(", ") + "))";
+		} else {
+			throw new Error("Unsupported geometry type: " + geoJson.geometry.type);
 		}
-	
-			// Optionally modify the request or options
-			const response = await originalFetch(resource, init);
-			if(isgeo)
-				{
-			// Intercept and optionally modify the response
-			const clonedResponse = response.clone();
-			clonedResponse.text().then((text) => {
-				console.log('Response:', text);
-			});
-		}
-	
-			return response;
-		};
-	})();
-*/
-
-function geoJsonToWKT(geoJson) {
-	if (geoJson.geometry.type === "Polygon") {
-	  // For Polygon, just join the coordinates in WKT format
-	  return "POLYGON ((" + geoJson.geometry.coordinates[0].map(coord => coord.join(" ")).join(", ") + "))";
-	} else if (geoJson.geometry.type === "MultiPolygon") {
-	  // For MultiPolygon, loop through each polygon and join coordinates in WKT format
-	  return "MULTIPOLYGON ((" + geoJson.geometry.coordinates.map(polygon => 
-		"(" + polygon[0].map(coord => coord.join(" ")).join(", ") + ")"
-	  ).join(", ") + "))";
-	} else {
-	  throw new Error("Unsupported geometry type: " + geoJson.geometry.type);
 	}
-  }
 
 
 	window.addEventListener('load', function () {
@@ -69,63 +39,69 @@ function geoJsonToWKT(geoJson) {
 
 		button.onclick = function (e) {
 
-			var location  =	JSON.parse(localStorage.getItem('LatestsSearches'))[0].combinedLocationIds.replace(/,/g, '_')
+			var location = JSON.parse(localStorage.getItem('LatestsSearches'))[0].combinedLocationIds.replace(/,/g, '_')
 
-		var url = "https://geom.fotocasa.es/v104/geom_" + location + ".js";
-		var xhr = new XMLHttpRequest();
+			var url = fetchurl.replace("[location]", location);
 
-		xhr.open('GET', url, true);
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState === XMLHttpRequest.DONE) {
-				if (xhr.status === 200) {
+			var xhr = new XMLHttpRequest();
 
-					var jsonstr  = xhr.responseText.replace(/(var(:?.*)geom_(:?.*)(?:\s)\=(?:\s))+/gm, "");
+			xhr.open('GET', url, true);
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState === XMLHttpRequest.DONE) {
+					if (xhr.status === 200) {
 
-					var feature = JSON.parse(jsonstr);
+						var jsonstr = xhr.responseText.replace(/(var(:?.*)geom_(:?.*)(?:\s)\=(?:\s))+/gm, "");
 
-					var wkt = geoJsonToWKT(feature);
-					console.log(wkt);
+						var feature = JSON.parse(jsonstr);
 
-					copyToCipboard(wkt);
+						var wkt = geoJsonToWKT(feature);
+						console.log(wkt);
 
-					var bc = document.querySelectorAll(".breadcrumb-navigation > li > a");
-					var cur = document.querySelector(".breadcrumb-navigation-current-level");
+						copyToCipboard(wkt);
 
-					if (bc.length === 0) {
-						bc = document.querySelectorAll(".breadcrumb-geo > ul > li > a");
-						cur = document.querySelector(".breadcrumb-geo > ul > li:last-child");
-					}
+						var bc = document.querySelectorAll(".breadcrumb-navigation > li > a");
+						var cur = document.querySelector(".breadcrumb-navigation-current-level");
 
-					var text = "Zona visível";
-					if (bc) {
-						var bcText = [];
-						bc.forEach(a => {
-							bcText.push(a.innerText);
-						});
-						if (cur) {
-							bcText.push(cur.innerText);
+						if (bc.length === 0) {
+							bc = document.querySelectorAll(".breadcrumb-geo > ul > li > a");
+							cur = document.querySelector(".breadcrumb-geo > ul > li:last-child");
 						}
-						if (bcText.length > 0) {
-							text = bcText.join(", ");
+
+						var text = "Zona visível";
+						if (bc) {
+							var bcText = [];
+							bc.forEach(a => {
+								bcText.push(a.innerText);
+							});
+							if (cur) {
+								bcText.push(cur.innerText);
+							}
+							if (bcText.length > 0) {
+								text = bcText.join(", ");
+							}
 						}
-					}
 
-					var msg = document.getElementById("map-undo-message");
-					if (msg) {
-						msg.remove();
-					}
+						var msg = document.getElementById("map-undo-message");
+						if (msg) {
+							msg.remove();
+						}
 
-					msg = document.createElement("div");
-					msg.id = "map-shape-message";
-					msg.classList.add("map-message");
-					msg.classList.add("feedback");
-					msg.classList.add("contextual");
-					msg.classList.add("warning");
-					msg.style.width = "100%";
-					msg.style.textAlign = "center";
-					msg.innerHTML = "SHAPE " + titleCase(text) + ", copiada para o clipboard<br />Verificar -> <a id='shapetest' href='" + validator + "' target='_shapetest'> Shape Test!</a>";
+						let element = document.getElementById("map-shape-message");
 
-					/*
+						if (element)
+							element.remove();
+
+						msg = document.createElement("div");
+						msg.id = "map-shape-message";
+						msg.classList.add("map-message");
+						msg.classList.add("feedback");
+						msg.classList.add("contextual");
+						msg.classList.add("warning");
+						msg.style.width = "100%";
+						msg.style.textAlign = "center";
+						msg.innerHTML = "SHAPE " + titleCase(text) + ", copiada para o clipboard<br />Verificar -> <a id='shapetest' href='" + validator + "' target='_shapetest'> Shape Test!</a>";
+
+						/*
 					var close = document.createElement("a");
 					close.classList.add("icon-close");
 					close.classList.add("close-btn");
@@ -138,24 +114,24 @@ function geoJsonToWKT(geoJson) {
 
 					msg.appendChild(close);
 */
-					var map = document.querySelector(".re-SearchMapWrapper");
-/*
-					var shapeclose = document.querySelector(".delete-cross-button.icon-close");
-					if (shapeclose) {
-						document.querySelector(".delete-cross-button.icon-close").onclick = closeMsg;
+						var map = document.querySelector(".re-SearchMapWrapper");
+						/*
+											var shapeclose = document.querySelector(".delete-cross-button.icon-close");
+											if (shapeclose) {
+												document.querySelector(".delete-cross-button.icon-close").onclick = closeMsg;
+											}
+						*/
+						map.insertBefore(msg, map.firstChild);
+
+
+					} else {
+						console.error('Request failed with status:', xhr.status);
 					}
-*/
-					map.insertBefore(msg, map.firstChild);
-
-
-				} else {
-					console.error('Request failed with status:', xhr.status);
 				}
-			}
-		};
-		xhr.send();
+			};
+			xhr.send();
 
-			
+
 
 			return false;
 		};
