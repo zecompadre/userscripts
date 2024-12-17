a59dc884848bad97187b2495730e8774d2d4322e
ZeCompadre <zecompadre@gmail.com>
2024-12-17 23:19:00 +0000

    Rename geojsonToWKT function to geojsonToWKTx and implement enhanced geometry handling

--- fotocasa.js
+++ fotocasa.js
@@ -2,7 +2,7 @@
 	const VALIDATOR_URL = "https://wkt-playground.zecompadre.com";
 	const FETCH_URL_BASE = "https://geom.fotocasa.es/v104/geom_";
 
-	function geojsonToWKT(geoJSON) {
+	function geojsonToWKTx(geoJSON) {
 		return geoJSON.features.map(({ geometry }) => {
 			switch (geometry.type) {
 				case "Polygon":
@@ -12,6 +12,42 @@
 						.map(polygon => `((${polygon[0].map(coord => coord.join(" ")).join(", ")
 							}))`)
 						.join(", ")})`;
+				default:
+					throw new Error(`Unsupported geometry type: ${geometry.type}`);
+			}
+		});
+	}
+
+	function geojsonToWKT(geoJSON) {
+		return geoJSON.features.map(({ geometry }) => {
+			switch (geometry.type) {
+				case "Polygon":
+					return `POLYGON ((${geometry.coordinates[0].map(coord => coord.join(" ")).join(", ")}))`;
+
+				case "MultiPolygon":
+					return `MULTIPOLYGON (${geometry.coordinates
+						.map(polygon => `((${polygon[0].map(coord => coord.join(" ")).join(", ")})`)
+						.join(", ")})`;
+
+				case "Point":
+					return `POINT (${geometry.coordinates.join(" ")})`;
+
+				case "MultiPoint":
+					return `MULTIPOINT (${geometry.coordinates.map(coord => `(${coord.join(" ")})`).join(", ")})`;
+
+				case "LineString":
+					return `LINESTRING (${geometry.coordinates.map(coord => coord.join(" ")).join(", ")})`;
+
+				case "MultiLineString":
+					return `MULTILINESTRING (${geometry.coordinates
+						.map(line => `(${line.map(coord => coord.join(" ")).join(", ")})`)
+						.join(", ")})`;
+
+				case "GeometryCollection":
+					return `GEOMETRYCOLLECTION (${geometry.geometries
+						.map(geom => geojsonToWKT({ features: [{ geometry: geom }] })[0])  // Recursive call for each geometry in the collection
+						.join(", ")})`;
+
 				default:
 					throw new Error(`Unsupported geometry type: ${geometry.type}`);
 			}
