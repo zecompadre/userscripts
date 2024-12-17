829c8187ce38ec3ab51917aa4c683d6eb9e49210
Luis Romão <luis.romao@janeladigital.com>
2024-12-17 09:12:27 +0000

    Refactor geojsonToWKT function for improved clarity and consistency in WKT conversion

--- fotocasa.js
+++ fotocasa.js
@@ -1,61 +1,61 @@
 (function () {
 
-	var validator = "https://wkt-playground.zecompadre.com";
-	//var validator = "https://wkt-plotter.zecompadre.com";
-	var fetchurl = "https://geom.fotocasa.es/v104/geom_";
+		var validator = "https://wkt-playground.zecompadre.com";
+		//var validator = "https://wkt-plotter.zecompadre.com";
+		var fetchurl = "https://geom.fotocasa.es/v104/geom_";
 
-	function geojsonToWKT(geoJson) {
+		function geojsonToWKT(geoJson) {
 
-		// Check if there's only one feature or more
-		if (geoJson.features.length === 1) {
-			// Single feature: Convert to WKT Polygon
-			const feature = geoJson.features[0];
-			if (feature.geometry.type === "Polygon") {
-				let coordinates = feature.geometry.coordinates[0];
-				let wktCoordinates = coordinates.map(coord => coord.join(' ')).join(', ');
-				return `POLYGON((${wktCoordinates}))`;
-			}
-		} else if (geoJson.features.length > 1) {
-			// Multiple features: Convert to WKT MultiPolygon
-			let multiPolygonCoordinates = geoJson.features.map(feature => {
+			// Check if there's only one feature or more
+			if (geoJson.features.length === 1) {
+				// Single feature: Convert to WKT Polygon
+				const feature = geoJson.features[0];
 				if (feature.geometry.type === "Polygon") {
 					let coordinates = feature.geometry.coordinates[0];
-					return `(${coordinates.map(coord => coord.join(' ')).join(', ')})`;
+					let wktCoordinates = coordinates.map(coord => coord.join(' ')).join(', ');
+					return `POLYGON((${wktCoordinates}))`;
 				}
-			}).join(', ');
+			} else if (geoJson.features.length > 1) {
+				// Multiple features: Convert to WKT MultiPolygon
+				let multiPolygonCoordinates = geoJson.features.map(feature => {
+					if (feature.geometry.type === "Polygon") {
+						let coordinates = feature.geometry.coordinates[0];
+						return `(${coordinates.map(coord => coord.join(' ')).join(', ')})`;
+					}
+				}).join(', ');
 
-			multiPolygonCoordinates = multiPolygonCoordinates.replace(/, , /g, ", ");
+				multiPolygonCoordinates = multiPolygonCoordinates.replace(/, , /g, ", ");
 
-			return `MULTIPOLYGON((${multiPolygonCoordinates}))`;
-		}
-		return null; // Return null if no valid Polygon or MultiPolygon features
-	}
-
-	window.addEventListener('load', function () {
-
-		// Save the original XMLHttpRequest open method
-		const originalOpen = XMLHttpRequest.prototype.open;
-
-		// Override the open method
-		XMLHttpRequest.prototype.open = function (...args) {
-			var loadGEOM = (args[1] || "").indexOf("/geom_") > -1;
-			if (!loadGEOM)
-				deleteElementOnAjax();
-
-			return originalOpen.apply(this, args); // Call the original method
-		};
-
-		// Function to delete the element
-		function deleteElementOnAjax() {
-			let element = document.getElementById("map-shape-message");
-			if (element)
-				element.remove();
+				return `MULTIPOLYGON((${multiPolygonCoordinates}))`;
+			}
+			return null; // Return null if no valid Polygon or MultiPolygon features
 		}
 
-		const container = document.createElement('div');
-		container.classList.add('fc-Save-search');
+		window.addEventListener('load', function () {
 
-		container.innerHTML = `<button id="btn-copy" aria-label="Copiar Shape" class="sui-AtomButton sui-AtomButton--primary sui-AtomButton--solid sui-AtomButton--center sui-AtomButton--fullWidth">
+				// Save the original XMLHttpRequest open method
+				const originalOpen = XMLHttpRequest.prototype.open;
+
+				// Override the open method
+				XMLHttpRequest.prototype.open = function (...args) {
+					var loadGEOM = (args[1] || "").indexOf("/geom_") > -1;
+					if (!loadGEOM)
+						deleteElementOnAjax();
+
+					return originalOpen.apply(this, args); // Call the original method
+				};
+
+				// Function to delete the element
+				function deleteElementOnAjax() {
+					let element = document.getElementById("map-shape-message");
+					if (element)
+						element.remove();
+				}
+
+				const container = document.createElement('div');
+				container.classList.add('fc-Save-search');
+
+				container.innerHTML = `<button id="btn-copy" aria-label="Copiar Shape" class="sui-AtomButton sui-AtomButton--primary sui-AtomButton--solid sui-AtomButton--center sui-AtomButton--fullWidth">
       <span class="sui-AtomButton-inner">
         <span class="sui-AtomButton-leftIcon">
           <div class="bell-animation">
@@ -70,93 +70,93 @@
       </span>
     </button>`;
 
-		const targetDiv = document.querySelector('.fc-Save-search'); // Replace '.existing-div' with the selector for your target div
+				const targetDiv = document.querySelector('.fc-Save-search'); // Replace '.existing-div' with the selector for your target div
 
-		targetDiv.insertAdjacentElement('afterend', container.firstElementChild);
+				targetDiv.insertAdjacentElement('afterend', container.firstElementChild);
 
-		var button = document.getElementById("btn-copy");
+				var button = document.getElementById("btn-copy");
 
-		button.onclick = async function (e) {
-			button.dataset.wkt = true;
+				button.onclick = async function (e) {
+					button.dataset.wkt = true;
 
-			// Get the latest searches from localStorage
-			const ids = JSON.parse(localStorage.getItem('LatestsSearches'))[0].combinedLocationIds.split(";");
+					// Get the latest searches from localStorage
+					const ids = JSON.parse(localStorage.getItem('LatestsSearches'))[0].combinedLocationIds.split(";");
 
-			const features = {
-				type: "FeatureCollection",
-				features: []
+					const features = {
+						type: "FeatureCollection",
+						features: []
+					};
+
+					const featurePromises = ids.map(async (id) => {
+						const location = id.replace(/,/g, '_');
+						const url = fetchurl + location + ".js";
+
+						try {
+							const response = await fetch(url);
+							if (!response.ok) {
+								console.error('Request failed with status:', response.status, 'for URL:', url);
+								return null; // Skip if fetch fails
+							}
+
+							let jsonstr = await response.text();
+							jsonstr = jsonstr.replace(/(var(:?.*)geom_(:?.*)(?:\s)\=(?:\s))+/gm, "");
+
+							try {
+								const feature = JSON.parse(jsonstr);
+								return feature; // Successfully parsed JSON
+							} catch (parseError) {
+								console.error('Failed to parse JSON:', parseError, 'for URL:', url);
+								return null;
+							}
+						} catch (error) {
+							console.error('Error fetching data for location', location, error);
+							return null; // Handle any errors in the fetch
+						}
+					});
+
+					// Wait for all fetches to complete
+					const featuresArray = await Promise.all(featurePromises);
+
+					// Add features, filtering out nulls
+					features.features.push(...featuresArray.filter(feature => feature !== null));
+					console.log('Features added successfully:', features.features);
+
+
+
+					console.log("features", features);
+
+					const wkt = geojsonToWKT(features);
+					console.log("wkt", wkt);
+
+					copyToCipboard(wkt);
+
+					// Remove existing messages
+					let msg = document.getElementById("map-undo-message");
+					if (msg) msg.remove();
+
+					let element = document.getElementById("map-shape-message");
+					if (element) element.remove();
+
+					// Create and display new message
+					msg = document.createElement("div");
+					msg.id = "map-shape-message";
+					msg.style.width = "100%";
+					msg.style.textAlign = "center";
+					msg.style.backgroundColor = "var(--c-secondary-dark-2)";
+					msg.style.color = "#fff";
+
+					msg.innerHTML = "SHAPE Zona visível, copiada para o clipboard<br />Verificar -> <a id='shapetest' href='" + validator + "' target='_shapetest' style='color: #fff; text-decoration: none; font-weight: 600;'> Shape Test!</a>";
+
+					const map = document.querySelector(".re-SearchMapWrapper");
+					map.insertBefore(msg, map.firstChild);
+
+				} catch (error) {
+					console.error('An error occurred:', error);
+				}
+
+				return false;
 			};
 
-			try {
-				// Iterate over each location id and fetch data asynchronously
-				const featurePromises = ids.map(async (id) => {
-					const location = id.replace(/,/g, '_');
-					const url = fetchurl + location + ".js";
-
-					try {
-						const response = await fetch(url);
-						if (!response.ok) {
-							console.error('Request failed with status:', response.status);
-							return null; // Return null or skip the current fetch if it fails
-						}
-
-						let jsonstr = await response.text();
-						jsonstr = jsonstr.replace(/(var(:?.*)geom_(:?.*)(?:\s)\=(?:\s))+/gm, "");
-
-						const feature = JSON.parse(jsonstr);
-
-
-
-						return feature;
-					} catch (error) {
-						console.error('Error fetching data for location', location, error);
-						return null; // Handle any errors in individual fetch requests
-					}
-				});
-
-				// Wait for all fetches to complete and filter out any failed responses (null values)
-				const featuresArray = await Promise.all(featurePromises);
-
-				setTimeout(function () {
-					features.features.push(...featuresArray.filter(feature => feature !== null));
-				}, 1000)
-
-
-
-				console.log("features", features);
-
-				const wkt = geojsonToWKT(features);
-				console.log("wkt", wkt);
-
-				copyToCipboard(wkt);
-
-				// Remove existing messages
-				let msg = document.getElementById("map-undo-message");
-				if (msg) msg.remove();
-
-				let element = document.getElementById("map-shape-message");
-				if (element) element.remove();
-
-				// Create and display new message
-				msg = document.createElement("div");
-				msg.id = "map-shape-message";
-				msg.style.width = "100%";
-				msg.style.textAlign = "center";
-				msg.style.backgroundColor = "var(--c-secondary-dark-2)";
-				msg.style.color = "#fff";
-
-				msg.innerHTML = "SHAPE Zona visível, copiada para o clipboard<br />Verificar -> <a id='shapetest' href='" + validator + "' target='_shapetest' style='color: #fff; text-decoration: none; font-weight: 600;'> Shape Test!</a>";
-
-				const map = document.querySelector(".re-SearchMapWrapper");
-				map.insertBefore(msg, map.firstChild);
-
-			} catch (error) {
-				console.error('An error occurred:', error);
-			}
-
-			return false;
-		};
-
-		//}, 3000);
-	}, false);
+			//}, 3000);
+		}, false);
 })();
