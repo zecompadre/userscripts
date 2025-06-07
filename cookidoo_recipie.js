747871b55c58f155af658131d5adbd14799102df
ZeCompadre <zecompadre@gmail.com>
2025-06-07 17:19:53 +0100

    Refactor extractSection function to include 'itens' parameter for improved data extraction and handling of 'also-featured-in-section'

--- cookidoo_recipie.js
+++ cookidoo_recipie.js
@@ -17,7 +17,7 @@
 		const title = document.querySelector(".recipe-card__section.recipe-card__name");
 		if (title) allText.push(title.innerText, '');
 
-		const extractSection = (id, inner, header) => {
+		const extractSection = (id, inner, itens, header) => {
 			const section = document.getElementById(id);
 			if (!section) return;
 
@@ -28,40 +28,51 @@
 
 				if (id === "tips-section") {
 					wrapper = group;
+				} else if (id === "also-featured-in-section") {
+					wrapper = group;
 				}
 
 				if (groupTitle) allText.push(groupTitle.innerText, '');
 
-				console.log(header, group, wrapper)
+				//console.log(header, group, wrapper, itens)
 
-				wrapper?.querySelectorAll("li").forEach(li => {
+				wrapper?.querySelectorAll(itens).forEach(li => {
 					let text = li.innerText.trim();
 					if (id === "ingredients-section") {
 						const name = li.querySelector(".recipe-ingredient__name")?.innerText.trim() || "";
 						const amount = li.querySelector(".recipe-ingredient__amount")?.innerText.trim() || "";
 						const description = li.querySelector(".recipe-ingredient__description")?.innerText.trim().replace(/[()]/g, "") || "";
 						text = `${amount} ${name}${description ? " (" + description + ")" : ""}`;
+					} else if (id === "also-featured-in-section") {
+						const name = section.querySelector(".rdp-collection-tile__name")?.innerText.trim() || "";
+						const countryHTML = section.querySelector(".rdp-collection-tile__info")?.innerHTML.trim() || "";
+						const countryName = countryHTML.split("<br>")[1]?.trim() || "Portugal";
+						text = `Cookidoo® ${countryName}: Colecção "${name}"`;
 					}
 					allText.push(text);
+					if (id === "tips-section") {
+						allText.push('');
+					}
 				});
 				allText.push('');
 			});
 		};
 
-		extractSection("ingredients-section", ".recipe-content__inner-section", "Ingredientes");
-		extractSection("preparation-steps-section", ".recipe-content__inner-section", "Preparação");
-		extractSection("tips-section", ".recipe-content__unordered-list", "Dicas");
+		extractSection("ingredients-section", ".recipe-content__inner-section", "li", "Ingredientes");
+		extractSection("preparation-steps-section", ".recipe-content__inner-section", "li", "Preparação");
+		extractSection("tips-section", ".recipe-content__unordered-list", "li", "Dicas");
+		extractSection("also-featured-in-section", "rdp-collections", "rdp-collection-tile", "Coleções");
 
-		const collections = document.getElementById("also-featured-in-section");
-		if (collections) {
-			allText.push('Coleções', '');
-			collections.querySelectorAll(".rdp-collection-tile__content").forEach(section => {
-				const name = section.querySelector(".rdp-collection-tile__name")?.innerText.trim() || "";
-				const countryHTML = section.querySelector(".rdp-collection-tile__info")?.innerHTML.trim() || "";
-				const countryName = countryHTML.split("<br>")[1]?.trim() || "Portugal";
-				allText.push(`Cookidoo® ${countryName}: Colecção "${name}"`);
-			});
-		}
+		// const collections = document.getElementById("also-featured-in-section");
+		// if (collections) {
+		// 	allText.push('Coleções', '');
+		// 	collections.querySelectorAll(".rdp-collection-tile__content").forEach(section => {
+		// 		const name = section.querySelector(".rdp-collection-tile__name")?.innerText.trim() || "";
+		// 		const countryHTML = section.querySelector(".rdp-collection-tile__info")?.innerHTML.trim() || "";
+		// 		const countryName = countryHTML.split("<br>")[1]?.trim() || "Portugal";
+		// 		allText.push(`Cookidoo® ${countryName}: Colecção "${name}"`);
+		// 	});
+		// }
 
 		console.log(allText.join('\n'));
 
